a4736832b1c06ddd00097ae73335b9e6d9266b63
D-setia <dsetia@cs.iitr.ac.in>
2020-06-30 10:06:56 +0530

    rfac: Add swiper for onboarding screens

--- codephile/lib/screens/on_boarding/on_boarding_template.dart
+++ codephile/lib/screens/on_boarding/on_boarding_template.dart
@@ -1,7 +1,5 @@
+import 'package:codephile/screens/login/login_screen.dart';
 import 'package:codephile/screens/on_boarding/bottom_info_display.dart';
-import 'package:codephile/screens/on_boarding/data.dart';
-import 'package:codephile/screens/on_boarding/on_boarding_background.dart';
-import 'package:codephile/screens/on_boarding/page_indicator.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingTemplate extends StatelessWidget {
@@ -20,38 +18,40 @@ class OnBoardingTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        OnBoardingBackground(),
         Scaffold(
           backgroundColor: Colors.transparent,
           body: Column(
             children: <Widget>[
               Expanded(
                   child: Padding(
-                padding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
-                child: Center(
-                  child: widgetToDisplay,
-                ),
-              )),
+                    padding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+                    child: Center(
+                      child: widgetToDisplay,
+                    ),
+                  )),
               BottomInfoDisplay(heading, description),
             ],
           ),
         ),
         Positioned(
-          left: MediaQuery.of(context).size.width / 25,
-          bottom: MediaQuery.of(context).size.height / 15,
-          child: PageIndicator(pageNo, noOfPages),
-        ),
-        Positioned(
             right: MediaQuery.of(context).size.width / 25,
             bottom: MediaQuery.of(context).size.height / 20,
-            child: FloatingActionButton(
+            child: (pageNo+1 == noOfPages)?
+            FloatingActionButton(
               backgroundColor: Colors.white,
               child: Icon(
                 Icons.arrow_forward,
                 color: Color.fromRGBO(35, 31, 32, 1),
               ),
-              onPressed: () => handleNextPress(pageNo, context),
-            ))
+              onPressed: (){
+                Navigator.of(context).popUntil((route) => route.isFirst);
+                Navigator.pushReplacement(context,
+                    new MaterialPageRoute(builder: (context) => new LoginScreen()));
+              },
+            )
+                :
+            Container()
+        )
       ],
     );
   }
