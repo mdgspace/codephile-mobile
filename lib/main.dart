697e3b5f831c69930e20500a2edc2d5ef150e70b
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-01-15 10:46:28 +0530

    conf: Set up Hive and HydratedBloc.

--- lib/main.dart
+++ lib/main.dart
@@ -5,67 +5,95 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'data/config/config.dart';
+import 'data/constants/strings.dart';
+import 'presentation/core/main_app.dart';
 
-Future<void> main() async {
+void main() {
+  // Zone for Sentry
   runZonedGuarded(
     () async {
       // Necessary if you intend to initialize in an async function.
       WidgetsFlutterBinding.ensureInitialized();
 
-      // Firebase section
-      await Firebase.initializeApp();
-      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-      FirebaseMessaging.onMessage.listen(
-        (message) {
-          final notification = message.notification;
-          if (notification != null) {
-            debugPrint(
-                'title: ${notification.title} \t body: ${notification.body}');
-          }
-        },
-      );
-      FirebaseMessaging.onMessageOpenedApp.listen(
-        (message) {
-          final notification = message.notification;
-          if (notification != null) {
-            debugPrint(
-              'title: ${notification.title} \t body: ${notification.body}',
-            );
-          }
-        },
-      );
+      _initOrientations();
+      await _initFirebase();
+      await _initSentry();
+      await _initHive();
+      final hydratedStorage = await _initHydratedBloc();
 
-      // Sentry section
-      await SentryFlutter.init(
-        (SentryFlutterOptions options) {
-          options
-            ..dsn = Environment.sentryDSN
-            ..tracesSampleRate = 1.0
-            ..reportPackages = true
-            ..debug = kDebugMode;
-        },
+      // Zone for Hydrated Bloc.
+      HydratedBlocOverrides.runZoned(
+        () async => runApp(await Codephile.run()),
+        storage: hydratedStorage,
       );
-
-      runApp(const Codephile());
     },
-    (Object exception, StackTrace stack) async => Sentry.captureException(
-      exception,
-      stackTrace: stack,
-    ),
+    _onError,
   );
 }
 
-class Codephile extends StatelessWidget {
-  const Codephile({Key? key}) : super(key: key);
+void _initOrientations() {
+  SystemChrome.setPreferredOrientations(
+    <DeviceOrientation>[DeviceOrientation.portraitUp],
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
+Future<void> _initFirebase() async {
+  await Firebase.initializeApp();
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  FirebaseMessaging.onMessage.listen(
+    (message) {
+      final notification = message.notification;
+      if (notification != null) {
+        debugPrint(
+            'title: ${notification.title} \t body: ${notification.body}');
+      }
+    },
+  );
+  FirebaseMessaging.onMessageOpenedApp.listen(
+    (message) {
+      final notification = message.notification;
+      if (notification != null) {
+        debugPrint(
+          'title: ${notification.title} \t body: ${notification.body}',
+        );
+      }
+    },
+  );
+}
+
+Future<void> _initSentry() async {
+  await SentryFlutter.init(
+    (SentryFlutterOptions options) {
+      options
+        ..dsn = Environment.sentryDSN
+        ..tracesSampleRate = 1.0
+        ..reportPackages = true
+        ..debug = kDebugMode;
+    },
+  );
+}
+
+Future<void> _initHive() async {
+  await Hive.initFlutter();
+  await Hive.openBox(AppStrings.hiveBoxName);
+}
+
+Future<Storage> _initHydratedBloc() async {
+  return HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationSupportDirectory(),
+  );
+}
+
+void _onError(Object exception, StackTrace stacktrace) async {
+  Sentry.captureException(exception, stackTrace: stacktrace);
 }
