ca3390eb6a55f15d530644715bc7b4c4a31dd2f4
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-01-15 17:20:54 +0530

    feat: Add basic services implementing `Dio` and `Hive`.

--- lib/data/config/config.dart
+++ lib/data/config/config.dart
@@ -1,3 +1,10 @@
 class Environment {
+  /// URL that is common to most outgoing requests.
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: 'https://codephile.mdg.iitr.ac.in/v1/',
+  );
+
+  /// Project identifier for Sentry error logging.
   static const String sentryDSN = String.fromEnvironment('SENTRY_DSN');
 }
