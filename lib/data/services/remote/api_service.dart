f2e27d1b23f20ca48624755a05f2b5b817b99e0a
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-02-01 12:09:38 +0530

    feat: Add logging to `ApiService`.

--- lib/data/services/remote/api_service.dart
+++ lib/data/services/remote/api_service.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:get/get.dart' as nav;
 
 import '../../config/config.dart';
+import '../../constants/routes.dart';
+import '../../constants/strings.dart';
 import '../local/storage_service.dart';
 
 class ApiService {
@@ -19,32 +24,29 @@ class ApiService {
     Map<String, dynamic>? headers,
     Map<String, dynamic>? query,
   }) async {
+    log('[API] [GET] >> $endpoint');
     Response? response;
     try {
       response = await _channel.get(
         (baseUrl ?? Environment.baseUrl) + endpoint,
         queryParameters: query,
         options: Options(
-          validateStatus: (status) {
-            return status! < 500;
-          },
+          validateStatus: _validate,
           headers: headers,
         ),
       );
-      return {
-        'status_code': response.statusCode ?? 0,
-        'data': response.data ?? 'null',
-      };
     } on Exception catch (exception, stacktrace) {
       debugPrint(
         'ERROR: Failed during a GET request.\n'
         'Endpoint: $endpoint\nQuery: $query\n'
         'Exception: $exception\nStacktrace: $stacktrace',
       );
+      rethrow;
     }
+    log('[API] [GET] << received ${response.statusCode} from $endpoint');
     return {
-      'status_code': response?.statusCode ?? 0,
-      'data': response?.data ?? 'null',
+      'status_code': response.statusCode ?? 0,
+      'data': response.data ?? 'null',
     };
   }
 
@@ -54,32 +56,29 @@ class ApiService {
     Map<String, dynamic>? headers,
     Map<String, dynamic>? data,
   }) async {
+    log('[API] [POST] >> $endpoint');
     Response? response;
     try {
       response = await _channel.post(
         Environment.baseUrl + endpoint,
         data: data,
         options: Options(
-          validateStatus: (status) {
-            return status! < 500;
-          },
+          validateStatus: _validate,
           headers: headers,
         ),
       );
-      return {
-        'status_code': response.statusCode ?? 0,
-        'data': response.data ?? 'null',
-      };
     } on Exception catch (exception, stacktrace) {
       debugPrint(
         'ERROR: Failed during a POST request.\n'
         'Endpoint: $endpoint\nData: $data\n'
         'Exception: $exception\nStacktrace: $stacktrace',
       );
+      rethrow;
     }
+    log('[API] [POST] << received ${response.statusCode} from $endpoint');
     return {
-      'status_code': response?.statusCode ?? 0,
-      'data': response?.data ?? 'null',
+      'status_code': response.statusCode ?? 0,
+      'data': response.data ?? 'null',
     };
   }
 
@@ -89,32 +88,29 @@ class ApiService {
     Map<String, dynamic>? headers,
     Map<String, dynamic>? data,
   }) async {
+    log('[API] [PUT] >> $endpoint');
     Response? response;
     try {
       response = await _channel.put(
         Environment.baseUrl + endpoint,
         data: data,
         options: Options(
-          validateStatus: (status) {
-            return status! < 500;
-          },
+          validateStatus: _validate,
           headers: headers,
         ),
       );
-      return {
-        'status_code': response.statusCode ?? 0,
-        'data': response.data ?? 'null',
-      };
     } on Exception catch (exception, stacktrace) {
       debugPrint(
         'ERROR: Failed during a PUT request.\n'
         'Endpoint: $endpoint\nData: $data\n'
         'Exception: $exception\nStacktrace: $stacktrace',
       );
+      rethrow;
     }
+    log('[API] [PUT] << received ${response.statusCode} from $endpoint');
     return {
-      'status_code': response?.statusCode ?? 0,
-      'data': response?.data ?? 'null',
+      'status_code': response.statusCode ?? 0,
+      'data': response.data ?? 'null',
     };
   }
 
@@ -122,5 +118,13 @@ class ApiService {
   static void addTokenToHeaders(Map<String, dynamic> headers) {
     final token = StorageService.authToken;
     headers.addAll({'authorization': token});
+  }
+
+  static bool _validate(int? status) {
+    if (status == 401) {
+      StorageService.delete(AppStrings.authTokenKey);
+      nav.Get.offAllNamed(AppRoutes.login);
+    }
+    return status! < 500;
   }
 }
