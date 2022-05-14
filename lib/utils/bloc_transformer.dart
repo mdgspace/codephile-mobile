c7237b4bdf1441c01afe1f83cc15dfe845e077c8
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-05-14 10:29:50 +0530

    ptch: Adjust debounce time in BLoC transformer.
    Signed-off-by: BURG3R5 <77491630+BURG3R5@users.noreply.github.com>

--- lib/utils/bloc_transformer.dart
+++ lib/utils/bloc_transformer.dart
@@ -5,7 +5,7 @@ import 'package:rxdart/rxdart.dart';
 Stream<T> Function(Stream<T>, Stream<T> Function(T)) getDebounce<T>() {
   return (Stream<T> events, Stream<T> Function(T) mapper) {
     return events
-        .debounceTime(const Duration(milliseconds: 200))
+        .debounceTime(const Duration(milliseconds: 100))
         .switchMap(mapper);
   };
 }
