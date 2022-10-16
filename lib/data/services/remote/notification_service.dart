c38a2a7b9aa124839cfb7d99aced3467d2eba5fe
Aman <aman2@me.iitr.ac.in>
2022-10-17 02:36:37 +0530

    bfix: resolve LocationNotFoundException
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

--- lib/data/services/remote/notification_service.dart
+++ lib/data/services/remote/notification_service.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 class NotificationService {
